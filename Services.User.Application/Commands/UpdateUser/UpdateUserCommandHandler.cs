7cd07597441a54bf2e34c6f01111d7c3fbf84061
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 16:49:17 -0300

    feat: Add Update user command

---
+++ Services.User.Application/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Services.User.Domain.Repositories;
+using Services.User.Domain.Services;
+
+namespace Services.User.Application.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler(IUserRepository userRepository, IUserService userService) : IRequestHandler<UpdateUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly IUserService _userService = userService;
+        public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userService.GetUserById(request.Id);
+
+            user.Update(request.Name, request.Password);
+
+            await _userRepository.UpdateAsync(user);
+
+            return Unit.Value;
+        }
+    }
+}
