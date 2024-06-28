2c79a413599591ff201b1d5c57a17a9c06ea485f
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 15:33:47 -0300

    test: Add Create user command unit test

---
+++ Services.User.Tests/Application/Commands/CreateUserCommandHandlerUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Services.User.Application.Commands.CreateUser;
+using Services.User.Domain.Repositories;
+using UserEntity = Services.User.Domain.Entities.User;
+
+namespace Services.Tests.Application.Commands
+{
+    [Collection(nameof(CreateUserCommandHandler))]
+    public class CreateUserCommandHandlerUnitTest
+    {
+        [Fact]
+        [Trait("Application", "CreateUser - Command")]
+        public async Task InputDataIsOk_Executed_ReturnOk()
+        {
+            var userRepository = new Mock<IUserRepository>();
+
+            var createUserCommand = new CreateUserCommand("John Doe", "john.doe@example.com", "p@ssw0rd");
+
+            var createUserCommandHandler = new CreateUserCommandHandler(userRepository.Object);
+
+            // Act
+            await createUserCommandHandler.Handle(createUserCommand, new CancellationToken());
+
+            // Assert
+            userRepository.Verify(u => u.AddAsync(It.IsAny<UserEntity>()), Times.Once);
+        }
+    }
+}
