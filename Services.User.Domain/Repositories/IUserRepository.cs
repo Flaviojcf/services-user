ee1d8b51dd020ab6b44a98eeaec627cb76caa2e7
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 14:26:39 -0300

    feat: Create user repository interface

---
+++ Services.User.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserEntity = Services.User.Domain.Entities.User;
+
+namespace Services.User.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity> GetByIdAsync(Guid id);
+        Task AddAsync(UserEntity user);
+        Task UpdateAsync(UserEntity user);
+        Task DeleteAsync(Guid id);
+    }
+}
