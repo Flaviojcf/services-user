e899f16c8d67f9b3acddd6f3268bd3fd8048ea4f
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 15:33:34 -0300

    feat: Add Create user command

---
+++ Services.User.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Services.User.Application.Commands.CreateUser
+{
+    public class CreateUserCommand(string name, string email, string password) : IRequest<Guid>
+    {
+        public string Name { get; set; } = name;
+        public string Email { get; set; } = email;
+        public string Password { get; set; } = password;
+    }
+}
