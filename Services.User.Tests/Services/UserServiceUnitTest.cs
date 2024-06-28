ee567a624be230049b6006fd2e8fa5290564c26d
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 16:50:15 -0300

    test: Add user service get by id unit test

---
+++ Services.User.Tests/Services/UserServiceUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Services.User.Application.Services;
+using Services.User.Domain.Services;
+using UserEntity = Services.User.Domain.Entities.User;
+
+namespace Services.Tests.Services
+{
+    [Collection(nameof(UserService))]
+    public class UserServiceUnitTest
+    {
+        [Fact]
+        [Trait("Application", "GetById - Service")]
+        public async Task InputDataIsOk_Executed_GetUser()
+        {
+            // Arrange
+            var userService = new Mock<IUserService>();
+
+            var user = new UserEntity("John Doe", "john.doe@example.com", "p@ssw0rd");
+            userService.Setup(s => s.GetUserById(user.Id)).ReturnsAsync(user);
+
+            // Act
+            var result = await userService.Object.GetUserById(user.Id);
+
+            // Assert
+            userService.Verify(u => u.GetUserById(user.Id), Times.Once);
+            Assert.Equal(user, result);
+        }
+
+    }
+}
