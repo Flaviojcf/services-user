aed14d58b639a98707760874a0016d20bef261d8
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 15:34:21 -0300

    feat: Add Delete user command unit test

---
+++ Services.User.Tests/Application/Commands/DeleteUserCommandHandlerUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using Services.User.Application.Commands.DeleteUser;
+using Services.User.Domain.Repositories;
+
+namespace Services.Tests.Application.Commands
+{
+
+    [Collection(nameof(DeleteUserCommandHandler))]
+    public class DeleteUserCommandHandlerUnitTest
+    {
+        [Fact]
+        [Trait("Application", "DeleteUser - Command")]
+        public async Task InputDataIsOk_Executed_ReturnOk()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var id = Guid.NewGuid();
+            var deleteUserCommand = new DeleteUserCommand(id);
+
+            var deleteUserCommandHandler = new DeleteUserCommandHandler(userRepository.Object);
+
+            // Act
+            await deleteUserCommandHandler.Handle(deleteUserCommand, new CancellationToken());
+
+            // Assert
+            userRepository.Verify(u => u.DeleteAsync(id), Times.Once);
+        }
+    }
+}
