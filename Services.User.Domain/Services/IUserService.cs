4d006cc51fa019db016bedd7e6160f3b32398300
Flávio Costa <flaviojcostafilho@gmail.com>
2024-06-28 16:50:36 -0300

    feat: Add User service

---
+++ Services.User.Domain/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using UserEntity = Services.User.Domain.Entities.User;
+
+namespace Services.User.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<UserEntity> GetUserById(Guid Id);
+    }
+}
